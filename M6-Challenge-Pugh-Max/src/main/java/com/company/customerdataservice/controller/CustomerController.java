be79d584a7cf27e293e56d00f8039d8004adfee0
Max Pugh <maxpugh@Maxs-MBP.lan>
2023-07-21 20:09:52 -0700

    established basic crud for customer

---
+++ M6-Challenge-Pugh-Max/src/main/java/com/company/customerdataservice/controller/CustomerController.java
@@ -0,0 +1,100 @@
+package com.company.customerdataservice.controller;
+
+
+import com.company.customerdataservice.model.Customer;
+import com.company.customerdataservice.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class CustomerController {
+
+
+    /*
+    As a user, I would like to be able to filter customers by state.
+
+    As a user, I would like to be able to find a specific customer by id.
+
+    As a user, I would like to be able to delete customers.
+
+    As a user, I would like to be able to edit customers.
+
+    As a user, I would like to be able to add new customers
+
+
+     */
+
+
+
+    @Autowired
+    CustomerRepository repo;
+
+//   C REATE
+
+//        As a user, I would like to be able to add new customers
+
+    @PostMapping("/customers")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Customer addCustomer(@RequestBody Customer customer){return repo.save(customer);}
+
+//    R EAD
+//    read all
+    @GetMapping("/customers")
+    @ResponseStatus(HttpStatus.OK)
+    public List<Customer> getCustomers(){return repo.findAll();}
+
+    @GetMapping("/customers/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Customer getCustomerById(@PathVariable int id){
+        Optional<Customer> toReturn = repo.findById(id);
+        if(toReturn.isPresent()){
+            return toReturn.get();
+        } else{
+            return null;
+        }
+    }
+
+
+
+//    U PDATE
+    @PutMapping("/customers/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public Customer updateCustomer(@PathVariable int id, @RequestBody Customer updatedCustomer) {
+        Optional<Customer> optionalCustomer = repo.findById(id);
+        if (optionalCustomer.isPresent()) {
+            Customer customerToUpdate = optionalCustomer.get();
+            customerToUpdate.setFirstName(updatedCustomer.getFirstName());
+            customerToUpdate.setLastName(updatedCustomer.getLastName());
+            customerToUpdate.setEmail(updatedCustomer.getEmail());
+            customerToUpdate.setCompany(updatedCustomer.getCompany());
+            customerToUpdate.setPhoneNumber(updatedCustomer.getPhoneNumber());
+            customerToUpdate.setAddress1(updatedCustomer.getAddress1());
+            customerToUpdate.setAddress2(updatedCustomer.getAddress2());
+            customerToUpdate.setCity(updatedCustomer.getCity());
+            customerToUpdate.setState(updatedCustomer.getState());
+            customerToUpdate.setPostalCode(updatedCustomer.getPostalCode());
+            customerToUpdate.setCountry(updatedCustomer.getCountry());
+            return repo.save(customerToUpdate);
+        } else {
+            return null;
+        }
+    }
+
+
+//    D ELETE
+    @DeleteMapping("customers/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteCustomer(@PathVariable int id){
+        repo.deleteById(id);
+    }
+
+
+
+
+//    Custom queries
+}
